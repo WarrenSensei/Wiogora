18d467a038c4c4216a1cef153de48e5c3a72a169
Ezra <ezra.davis@gmail.com>
2016-02-20 18:25:19 -0500

    Style changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	def active_class(link_path)
+		current_page?(link_path) ? "active" : ""
+	end
+
+	# Code from http://stackoverflow.com/questions/17481812/dynamically-add-active-class-to-bootstrap-li-in-rails
+	def link_to_in_li(text, url, html_options = {})
+		content_tag :li, class: active_class(url) do
+			link_to text, url, html_options
+		end
+	end
 end
